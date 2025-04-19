0104897457bfa1e8a16beecb1432dbb0b466cd60
SkipEveryLunch <skipeverylunch@outlook.jp>
2025-04-19 09:22:58 +0000

    fix: CodeBuildプロジェクトにprivileged modeを追加

--- terraform/codepipeline.tf
+++ terraform/codepipeline.tf
@@ -20,6 +20,7 @@ resource "aws_codebuild_project" "build" {
     image                       = "aws/codebuild/standard:5.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
 
     environment_variable {
       name  = "AWS_DEFAULT_REGION"
